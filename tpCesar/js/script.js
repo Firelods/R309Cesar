4197dd827827e2c617208568a921a20169f1078a
lc101219 <clement.lefevre1@etu.univ-cotedazur.fr>
2022-10-21 15:33:15 +0200

    update avec lecture fichier

--- tpCesar/js/script.js
+++ tpCesar/js/script.js
@@ -3,6 +3,7 @@ function CesarCode(event) {
   const results = document.querySelector("#results");
   const decalage = parseInt(document.querySelector("#decalage").value);
   const bruteForce = document.querySelector("#bruteForce").checked;
+  probCharCorpus("A");
   console.log(bruteForce);
   if (inputATransform.checkValidity()) {
     var textInput = inputATransform.value;
@@ -28,4 +29,54 @@ function transformToCesar(textInput, decalage) {
     );
   }
   return result;
+}
+
+function readTextFile(file) {
+  var dict = {};
+  var rawFile = new XMLHttpRequest();
+  rawFile.open("GET", file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        allText.split(/\r?\n/).forEach((line) => {
+          var lineSplitted = line.split(" ");
+          if (lineSplitted.length > 1) {
+            dict[lineSplitted[0]] = lineSplitted[1];
+          }
+        });
+        console.log(dict);
+        return dict;
+      }
+    }
+  };
+  rawFile.send(null);
+}
+
+function turnDictInProb(dict) {
+  var sum = 0;
+  for (var key in dict) {
+    sum += parseInt(dict[key]);
+    console.log(dict[key]);
+  }
+  console.log(sum);
+  for (var key in dict) {
+    dict[key] = parseInt(dict[key]) / sum;
+  }
+  return dict;
+}
+
+function charProbInMsg(char, msg) {
+  var appear = 0;
+  for (var i = 0; i < msg.length; i++) {
+    if (msg[i] == char) {
+      appear++;
+    }
+  }
+  return appear / msg.length;
+}
+
+function probCharCorpus(char) {
+  var dict = readTextFile("assets/frequence_francais.txt");
+  console.log(dict);
 }
